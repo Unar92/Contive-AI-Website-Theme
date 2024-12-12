a9dd28d9c7700e10c17e429d8b079323fd0f896d
Unar <129647391+Unar92@users.noreply.github.com>
2024-12-12 18:57:30 +0500

    added initial home page

---
+++ src/assets/js/zscript.js
@@ -0,0 +1,22 @@
+// function to toggle class to c-contact-us is--active when click on .js-contact-open
+// function to toggle class to c-contact-us is--active when click on .js-contact-close
+document.querySelector('.js-contact-open').addEventListener('click', function() {
+
+
+    //hide .byVismeButton--PYBh3EN and .byVismeButton--PYBh3EN.mobile--dwFnSiL than proceed to next step
+    if (document.querySelector('.byVismeButton--PYBh3EN')) {
+        document.querySelector('.byVismeButton--PYBh3EN').style.display = 'none';
+        console.log('byVismeButton--PYBh3EN hidden');
+    }
+       
+    document.querySelector('.c-contact-us').classList.toggle('is--active');
+
+
+
+});
+
+
+
+document.querySelector('.js-contact-close').addEventListener('click', function() {
+    document.querySelector('.c-contact-us').classList.toggle('is--active');
+});
